77b311dfb0e4cabd02fcc369ea1e9395423a4ad1
pixelchutes <mike@pixelchutes.com>
2012-11-07 22:19:18 -0700

    Redundant session.isMustacheAficionado, and image block id

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.configure(function(){
 //admin/testing routes
 app.get('/images/delete/:imageId', session.isMustacheAficionado, session.requiresLogin, routes.deleteImage);
 app.get('/user/generate', session.isMustacheAficionado, session.requiresLogin, user.generateGifFromSequence);
-app.get('/user/images/delete', session.isMustacheAficionado, session.isMustacheAficionado, session.requiresLogin, user.deleteAllImages);
+app.get('/user/images/delete', session.isMustacheAficionado, session.requiresLogin, user.deleteAllImages);
 app.get('/user/animations/delete', session.isMustacheAficionado, session.requiresLogin, user.deleteAllGifs);
 app.get('/upload', session.isMustacheAficionado, session.requiresLogin, upload.uploadPage);
 
